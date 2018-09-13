dabda4ed831d96e011243d2e10465091eadf41ef
gio13k <gio13k@infonie.fr>
2018-09-13 16:29:18 +0200

    close #1 display yellow number above 150 was fixed.

--- ExoAleatoire.php
+++ ExoAleatoire.php
@@ -1,10 +1,39 @@
+
 <?php
 
 $valeurRecherchee = 155;
 $compteur = 1;
+$changeStyle = "";
 while(rand(0,1000)!== $valeurRecherchee){
     $compteur++; 
 }
-echo "Il a fallu $compteur fois pour trouver $valeurRecherchee !";
+$message =  "Il a fallu $compteur fois pour trouver $valeurRecherchee !";
+
+if ($compteur > 150){
+    $changeStyle = "class = 'message'";
+}
 
 ?>
+
+<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style> 
+    .message{
+        background-color : yellow;
+        border : solid;
+        border-radius: 5px;
+        } 
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <p> <span <?php echo  $changeStyle ?>  ><?php echo  $message ?> </span></p>
+</body>
+</html>
+
+</html>
